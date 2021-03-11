b1463d6da45bb1616bbb327cd8cc0c39f31cd198
Wstld <none@none.com>
2021-03-11 11:55:34 +0100

    menu functionallity added, sound added.

--- script.js
+++ script.js
@@ -1,40 +1,80 @@
+//weapon selected
+var weaponOfChoice = ""
+//weapons
+const LIGHTNING = "LIGHTNING"
+const SQUISH = "SQUISH"
+const EXPLODE = "EXPLODE"
 
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
 
 //setup canvas 
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
+//menu btns
+const explodeBtn = document.getElementById('explode')
+const thunderBoltBtn = document.getElementById('lightning')
+const squishBtn = document.getElementById('squish')
+
+//image object holds all sprites.
 const images = {};
+
+
 const bunnyActions = ['up','down','left','right','still']
+
+//arrays to hold sprite objects.
 const bunnysArr = []
 const effectsArr = []
+
+//add sprites to images object.
 images.bunny = new Image();
 images.bunny.src = 'rabbit.png';
 images.explosion = new Image();
 images.explosion.src = 'explosion.png'
+images.lightning = new Image();
+images.lightning.src = 'lightning2.png'
+images.squish = new Image()
+images.squish.src = 'squish.png'
+
+
 
 class Effect {
-    constructor(x,y){
+    constructor(x,y,width,height,frameX,frameY,img,sound){
         //explosiion effect.
-        this.width = 64;
-        this.height = 64;
-        this.frameX = 0;
-        this.frameY = 0;
+        this.width = width;
+        this.height = height;
+        this.frameX = frameX;
+        this.frameY = frameY;
         this.x = x
         this.y = y
         this.finnished = false
+        this.image = img
+        this.sound = sound
+    }
+    resetEffect(){
+        this.finnished = false
+        this.frameX = 0;
+        this.frameY = 0;
+    }
+    setPosition(x,y){
+        this.x = x;
+        this.y = y;
+    }
+    playSoundEffect(){
+        this.sound.play()
     }
     draw(){
-        drawSprite(images.explosion, this.width * this.frameX,this.height * this.frameY,this.width,this.height,this.x,this.y,this.width,this.height)
+        drawSprite(this.image, this.width * this.frameX,this.height * this.frameY,this.width,this.height,this.x,this.y,this.width,this.height)
 };
 update(){
     if (this.frameX < 7){
         this.frameX ++
+        console.log("x:"+this.frameX)
     }else{
         if(this.frameY < 3){
             this.frameY ++
+            console.log("y:"+this.frameY)
             this.frameX = 0
         }else{
             this.finnished = true;
@@ -43,7 +83,13 @@ update(){
     }
 }
 
-}
+};
+
+//effect object
+const effects = {}
+effects.explosion = new Effect(0,0,64,64,0,0,images.explosion,new Audio('sound/explosion.flac'))
+effects.lightning = new Effect(0,0,72,72,0,0,images.lightning,new Audio('sound/thunder.flac'))
+effects.squish = new Effect(0,0,72,72,0,0,images.squish,new Audio('sound/squish.flac'))
 
 class Bunny {
     constructor(){
@@ -142,6 +188,7 @@ class Bunny {
 
 
 };
+
 for (let i = 0; i < 10; i++) {
     bunnysArr.push(new Bunny())
 }
@@ -189,18 +236,72 @@ function animate(){
         
     }
 };
+//attack bunny -- you heartless bastard.
+function attackBunny(bunny,index){
+    if(weaponOfChoice == EXPLODE){
+    effects.explosion.resetEffect()
+    effects.explosion.setPosition(bunny.x,bunny.y);
+    effects.explosion.sound.play()
+    effectsArr.push(effects.explosion)
+    bunnysArr.splice(index,1)
+    }else if(weaponOfChoice == LIGHTNING) {
+        effects.lightning.resetEffect()
+        effects.lightning.setPosition(bunny.x,bunny.y);
+        effects.lightning.sound.play()
+        effectsArr.push(effects.lightning)
+        bunnysArr.splice(index,1)
+    }else if(weaponOfChoice == SQUISH) {
+        effects.squish.resetEffect()
+        effects.squish.setPosition(bunny.x,bunny.y);
+        effects.squish.sound.play()
+        effectsArr.push(effects.squish)
+        bunnysArr.splice(index,1)
+    }else{
+        console.log("nothing")
+    }
+}
+//on bunny clicked.
 canvas.addEventListener('click',function(event){
     for (let i = 0; i < bunnysArr.length; i++) {
         if(rectIntersect(event.clientX,event.clientY,10,10,bunnysArr[i].x + 30 ,bunnysArr[i].y + 30, 7, 7) == true){
-           effectsArr.push(new Effect(bunnysArr[i].x,bunnysArr[i].y))
-           console.log(bunnysArr)
-            bunnysArr.splice(i,1)
-            console.log(bunnysArr)
+           attackBunny(bunnysArr[i],i)
         }
         
     }
  
 })
+//on menu clicked
+explodeBtn.addEventListener('click',function(){
+    resetBtnBg(explodeBtn);
+    explodeBtn.style.backgroundColor = 'lightskyblue';
+    weaponOfChoice = EXPLODE;
+})
+thunderBoltBtn.addEventListener('click',function(){
+    resetBtnBg(thunderBoltBtn);
+    thunderBoltBtn.style.backgroundColor = 'lightskyblue';
+    weaponOfChoice = LIGHTNING;
+})
+squishBtn.addEventListener('click',function(){
+    resetBtnBg(squishBtn);
+    squishBtn.style.backgroundColor = 'lightskyblue';
+    weaponOfChoice = SQUISH;
+})
+
+//reset btn colors
+function resetBtnBg(btn){
+    if(btn == thunderBoltBtn){
+        squishBtn.style.backgroundColor = 'whitesmoke'
+        explodeBtn.style.backgroundColor = 'whitesmoke'
+    }else if (btn == explodeBtn){
+        squishBtn.style.backgroundColor = 'whitesmoke'
+        thunderBoltBtn.style.backgroundColor = 'whitesmoke'
+    }else{
+        thunderBoltBtn.style.backgroundColor = 'whitesmoke'
+        explodeBtn.style.backgroundColor = 'whitesmoke'
+    }
+}
+
+
 window.onload = setInterval(animate,1000/10)
 window.addEventListener('resize',function(){
     canvas.height = window.innerHeight
